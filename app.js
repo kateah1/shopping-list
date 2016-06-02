eda3ded032dd42314b660d40bf85669b68df69a3
kateah1 <kate.hurd64@gmail.com>
2016-06-02 14:13:00 -0400

    212

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ $(document).ready(function() {
 	});
 	*/
 	
+	//use css to show mouse hovering over submit button
+
 	$("input[type=submit").mouseenter(function() {
 		$(this).css({
 			'cursor', 'pointer',
@@ -35,11 +37,16 @@ $(document).ready(function() {
 		});
 	});
 
+	//add list item after userInput and pressing submit
+
 	document.getElementById("add").onclick = function() {
+		var img = <img src="https://cdn3.iconfinder.com/data/icons/cleaning-icons/512/Trash_Can-512.png">
 		var text = document.getElementById("item").value;
-		var li = "<li>" + text + "</li>";
+		var li = img + "<li>" + text + "</li>";
 		document.getElementById("list").appendChild(li);
 	};
+
+	//delete list item by clicking on trash icon
 
 	$("img").hover(function()	{
 		$(this).css('cursor', 'pointer',);
@@ -48,6 +55,8 @@ $(document).ready(function() {
 		//remove <li> below
 		$("li").remove();
 	});
+
+	//check list items on and off using strikethrough by clicking on them
 
 	$("li").toggle(function() {
 		$(this).css('text-decoration' : 'line-through');
